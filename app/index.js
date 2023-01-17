41c997cad8b9e48519f547deb49dbda8423b8d8e
Aundrei Bryant <drei97@gmail.com>
2023-01-17 10:05:23 -0600

    refactored: get method

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 const app = express();
-app.get =
-  ("/",
-  (_, response) => {
-    response.send("<h1> hello </h1>");
-  });
+app.get("/", (_, response) => {
+  response.send("<h1> hello world </h1>");
+});
 
 app.listen(3000, () => {
   console.info("server is running on port 3000");
+});
+
+app.get("/about", (_, response) => {
+  response.send("<h1> hello world </h1>");
 });
