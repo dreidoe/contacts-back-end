854a7eece4a4bfa730fbe2c4f485c49e83810e41
Aundrei Bryant <drei97@gmail.com>
2023-01-17 11:10:08 -0600

    WIP

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 const app = express();
 app.get("/", (_, response) => {
   response.send("<h1> hello world </h1>");
@@ -11,6 +12,11 @@ app.listen(3000, () => {
 app.get("/about", (_, response) => {
   response.send("<h1> hello world </h1>");
 });
+
+app.get("/api/contacts/:id", (request, response) => {
+  console.log(request.params.id);
+  response.send(request.params.id);
+});
 // Express uses middleware to parse the body of a request
 // this is required to access the body of a POST request
 app.use(express.json());
@@ -18,4 +24,11 @@ app.use(express.json());
 app.post("/api/contacts", (request, response) => {
   console.log(request.body);
   response.send("ok");
+});
+
+app.get("/api/contacts", (_, response) => {
+  response.json([
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+  ]);
 });
