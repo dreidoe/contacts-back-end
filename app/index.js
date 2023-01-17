b130b5d62a0bba53fc56b0807d909a14cdba3512
Aundrei Bryant <drei97@gmail.com>
2023-01-17 10:28:30 -0600

    created comment and middleware  to read in json

--- app/index.js
+++ app/index.js
@@ -11,3 +11,11 @@ app.listen(3000, () => {
 app.get("/about", (_, response) => {
   response.send("<h1> hello world </h1>");
 });
+// Express uses middleware to parse the body of a request
+// this is required to access the body of a POST request
+app.use(express.json());
+
+app.post("/api/contacts", (request, response) => {
+  console.log(request.body);
+  response.send("ok");
+});
